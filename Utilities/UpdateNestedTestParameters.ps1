305f3f3753d0291925cb49eb2deb7b0475515c23
andylei <378513086@qq.com>
2018-07-23 15:02:28 +0800

    add a script for update nested test parameters

---
+++ Utilities/UpdateNestedTestParameters.ps1
@@ -0,0 +1,40 @@
+param(
+    [string]$NestedImageUrl= "",
+    [string]$NestedUser= "",
+    [string]$NestedUserPassword= "",
+    [string]$RaidOption= ""
+)
+
+
+$TestXMLs = Get-ChildItem -Path ".\XML\TestCases\*.xml"
+foreach ( $file in $TestXMLs.FullName)
+{
+	$TestXmlConfig = [xml]( Get-Content -Path $file)
+	foreach ( $test in $TestXmlConfig.TestCases.test )
+	{
+		if ( $test.Area -eq 'Nested' )
+		{
+			Write-Host "Update test parameters for case $($test.TestName)"
+			foreach ($param in $test.TestParameters.ChildNodes)
+			{
+				if ( $param."#text" -match 'NestedImageUrl=' )
+				{
+					$param."#text" = "NestedImageUrl=$NestedImageUrl"
+				}
+				if ( $param."#text" -match 'NestedUser=' )
+				{
+					$param."#text" = "NestedUser=$NestedUser"
+				}
+				if ( $param."#text" -match 'NestedUserPassword=' )
+				{
+					$param."#text" = "NestedUserPassword='$NestedUserPassword'"
+				}
+				if ( $param."#text" -match 'RaidOption=' )
+				{
+					$param."#text" = "RaidOption='$RaidOption'"
+				}
+			}
+		}
+	}
+	$TestXmlConfig.save($file)
+}
