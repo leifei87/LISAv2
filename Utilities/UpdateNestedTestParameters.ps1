0043a40e896e5a7c5006bd6ae7bf243f0551eb78
andylei <378513086@qq.com>
2018-07-25 10:48:39 +0800

    support to update test parameters for multiple cases

--- Utilities/UpdateNestedTestParameters.ps1
+++ Utilities/UpdateNestedTestParameters.ps1
@@ -1,3 +1,25 @@
+##############################################################################################
+# UpdateNestedTestParameters.ps1
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License.
+<#
+.SYNOPSIS
+    Update test parameters for nested kvm test case
+
+.PARAMETER 
+    <Parameters>
+
+.INPUTS
+
+
+.NOTES
+    Creation Date:  23th July 2018
+
+.EXAMPLE
+
+#>
+###############################################################################################
+
 param(
     [string]$TestName= "",
     [string]$NestedImageUrl= "",
@@ -6,14 +28,13 @@ param(
     [string]$RaidOption= ""
 )
 
-
 $TestXMLs = Get-ChildItem -Path ".\XML\TestCases\*.xml"
 foreach ( $file in $TestXMLs.FullName)
 {
 	$TestXmlConfig = [xml]( Get-Content -Path $file)
 	foreach ( $test in $TestXmlConfig.TestCases.test )
 	{
-		if ( $test.Area -eq 'Nested' -and $test.TestName -eq $TestName )
+		if ( $test.Area -eq 'Nested' -and $TestName.Split(',').contains($($test.TestName)) )
 		{
 			Write-Host "Update test parameters for case $($test.TestName)"
 			foreach ($param in $test.TestParameters.ChildNodes)
